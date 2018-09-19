76126d9d2f285deace746653bd3eaa755a9c2182
Khoi Le <minarek@outlook.com>
2018-09-19 15:34:33 +0200

    Fixed bugs

--- webpack.custom.js
+++ webpack.custom.js
@@ -3,10 +3,6 @@ module.exports = {
         index: './src/index.js', // default
         home: './src/components/home.js', // change to your another-module.js here
         product: './src/components/product.js',
-        productlist: './src/components/product-list.js',
-        cart: './src/components/cart.js',
-        order: './src/components/order.js',
-        review: './src/components/review.js'
     },
     htmlTemplate: './src/index.html',
     distDir: './dist', // Distribution directory
